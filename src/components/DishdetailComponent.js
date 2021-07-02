6f421d836642cd3473410557f6a363d39b8d00c0
Naman Chanduka <namanchanduka2001@gmail.com>
2021-07-02 17:54:11 +0530

    Assignment 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  renderComments(comms) {
+    if (comms != null) {
+      const commentList = comms.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <p>{comment.comment}</p>
+            <p>
+              -- {comment.author}, &nbsp;
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "2-digit",
+              }).format(new Date(comment.date))}
+            </p>
+          </li>
+        );
+      });
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">{commentList}</ul>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  render() {
+    if (this.props.selectedDish != null) {
+      return (
+        <div className="row">
+          {this.renderDish(this.props.selectedDish)}
+          {this.renderComments(this.props.selectedDish.comments)}
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+}
+
+export default DishDetail;
