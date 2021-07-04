0d54b39dc6fff8f78109ea9914976a297d45d2bb
Naman Chanduka <namanchanduka2001@gmail.com>
2021-07-04 19:38:25 +0530

    Single Page Applications

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,7 +6,10 @@ import {
   CardText,
   CardBody,
   CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
 } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
   if (dish != null) {
@@ -59,8 +62,20 @@ const DishDetail = (props) => {
     return (
       <div className="container">
         <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.selectedDish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.selectedDish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
           <RenderDish dish={props.selectedDish} />
-          <RenderComments comms={props.selectedDish.comments} />
+          <RenderComments comms={props.comments} />
         </div>
       </div>
     );
