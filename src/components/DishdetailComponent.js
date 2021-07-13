e811f950d8d3033147d2d524c9d34ef2ae58dba2
Naman Chanduka <namanchanduka2001@gmail.com>
2021-07-13 21:53:28 +0530

    fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,6 +19,7 @@ import {
 import { Link } from "react-router-dom";
 import { Errors, LocalForm, Control } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -28,7 +29,12 @@ function RenderDish({ dish }) {
     return (
       <div className="col-12 col-md-5 m-1">
         <Card>
-          <CardImg width="100%" object src={dish.image} alt={dish.name} />
+          <CardImg
+            width="100%"
+            object
+            src={baseUrl + dish.image}
+            alt={dish.name}
+          />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,6 +8,7 @@ import {
   CardSubtitle,
 } from "reactstrap";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 function RenderCard({ items, isLoading, errMess }) {
   if (isLoading) {
@@ -17,7 +18,7 @@ function RenderCard({ items, isLoading, errMess }) {
   } else {
     return (
       <Card>
-        <CardImg src={items.image} alt={items.name} />
+        <CardImg src={baseUrl + items.image} alt={items.name} />
         <CardBody>
           <CardTitle>{items.name}</CardTitle>
           {items.designation ? (
@@ -42,7 +43,11 @@ function Home(props) {
           />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard items={props.promotion} />
+          <RenderCard
+            items={props.promotion}
+            isLoading={props.promosLoading}
+            errMess={props.promosErrMess}
+          />
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard items={props.leader} />
